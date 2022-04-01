bec9711cb6ba9e0747b96628b6642b1a9514554e
Diego Chavez <ingdiegochavez18@gmail.com>
2022-03-31 23:12:45 -0500

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<ion-app>
+		<ion-toolbar color="dark">
+			<ion-title>Default Buttons</ion-title>
+		</ion-toolbar>
+
+		<ion-toolbar>
+			<nav>
+				<router-link to="/">Home</router-link> |
+				<router-link to="/about">About</router-link>
+			</nav>
+		</ion-toolbar>
+
+		<ion-content>
+			<router-view />
+		</ion-content>
+	</ion-app>
+</template>
+
+<script>
+import { IonApp, IonTitle, IonToolbar, IonContent } from "@ionic/vue";
+export default {
+	components: { IonApp, IonTitle, IonToolbar, IonContent },
+};
+</script>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<ion-card v-for="post in posts" :key="post.data.id" color="dark">
+			<ion-card-header>
+				<ion-image :src="post.data.thumbnail" />
+				<ion-card-title>{{ post.data.title }}</ion-card-title>
+			</ion-card-header>
+			<ion-card-content>
+				<p>{{ post.data.content }}</p>
+			</ion-card-content>
+		</ion-card>
+	</div>
+</template>
+
+<script>
+import axios from "axios";
+import {
+	IonCard,
+	IonCardHeader,
+	IonCardTitle,
+	IonCardContent,
+	IonImage,
+} from "@ionic/vue";
+export default {
+	components: { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImage },
+	data() {
+		return {
+			posts: [],
+		};
+	},
+	async mounted() {
+		const response = await axios.get(
+			"https://www.reddit.com/r/marvelstudios.json"
+		);
+		this.posts = response.data.data.children;
+	},
+};
+</script>
+
+<style></style>
